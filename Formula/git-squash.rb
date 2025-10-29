baa39d8d42aa0fe818d1a7a6ad9ea4a75ca766ef
zekun <zekunzhan@tencent.com>
2025-10-29 15:26:49 +0800

    Update Formula/git-squash.rb to v1.0.1

--- Formula/git-squash.rb
+++ Formula/git-squash.rb
@@ -1,8 +1,8 @@
 class GitSquash < Formula
   desc "Locally squash commits on a branch, without needing to resolve any conflicts"
-  homepage "https://github.com/zhanzekun/git-squash"
-  url "https://github.com/zhanzekun/git-squash/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "TBD" # Run: curl -L https://github.com/zhanzekun/git-squash/archive/refs/tags/v1.0.0.tar.gz | sha256sum
+  homepage "https://github.com/zhanzekun/homebrew-git-squash"
+  url "https://github.com/zhanzekun/homebrew-git-squash/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "53af50c9c8e3c6e1517be1ff41d82a022d51ca95d851001b4eee82c43731bd47"
   license "MIT"
 
   def install
