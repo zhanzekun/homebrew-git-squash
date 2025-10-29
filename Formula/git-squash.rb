04767a40568c34dcbb92a3141cd539beec926ffd
zekun <zekunzhan@tencent.com>
2025-10-29 15:27:20 +0800

    Fix SHA256 for v1.0.1

--- Formula/git-squash.rb
+++ Formula/git-squash.rb
@@ -2,7 +2,7 @@ class GitSquash < Formula
   desc "Locally squash commits on a branch, without needing to resolve any conflicts"
   homepage "https://github.com/zhanzekun/homebrew-git-squash"
   url "https://github.com/zhanzekun/homebrew-git-squash/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "53af50c9c8e3c6e1517be1ff41d82a022d51ca95d851001b4eee82c43731bd47"
+  sha256 "7c90fe3f47cb2f8e8fc293276bb8d1c885d543a33ab8bf4d0bfdd46fc1693a90"
   license "MIT"
 
   def install
